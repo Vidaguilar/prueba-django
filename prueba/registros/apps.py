b63bc35326a37576fc2f141a5fbc4683b5764f65
Vidaguilar <vidaguilar22102001@gmail.com>
2022-06-27 19:01:34 -0500

    agregada Confifuracion admin

---
+++ prueba/registros/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class RegistrosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'registros'
+    verbose_name='Módulos'
