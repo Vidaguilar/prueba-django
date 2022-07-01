b1dc18ef1bee714483ad929c087a9c8682ae8b64
unknown <vidaguilar22102001@gmailcom>
2022-07-01 07:58:54 -0500

    Comit

--- prueba/registros/models.py
+++ prueba/registros/models.py
@@ -1,4 +1,6 @@
+from sre_parse import Verbose
 from django.db import models
+from ckeditor.fields import RichTextField
 class Alumnos(models.Model): #Define la estructura de nuestra tabla
     matricula = models.CharField(max_length=12,verbose_name="Mat") #Texto corto
     nombre = models.TextField() #Texto largo
@@ -15,3 +17,17 @@ class Alumnos(models.Model): #Define la estructura de nuestra tabla
          return self.nombre
 
 # Create your models here.
+
+class Comentario(models.Model):
+     id=models.AutoField(primary_key=True,verbose_name="clave")
+     alumno=models.ForeignKey(Alumnos,on_delete=models.CASCADE,verbose_name="Alumno")
+     created = models.DateField(auto_now_add=True,verbose_name="Registrado")
+     coment = RichTextField(verbose_name="Comentario")
+     class Meta:
+         verbose_name = "Comentario"
+         verbose_name_plural = "Comentarios"
+         ordering = ["-created"] 
+     def __str__(self):
+         return self.coment
+
+     
