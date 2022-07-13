e0c44ea6849876febff4fe16f002bb62928b6369
unknown <vidaguilar22102001@gmailcom>
2022-07-13 08:36:32 -0500

    ya puedo editar bien 3

--- prueba/registros/admin.py
+++ prueba/registros/admin.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.contrib import admin
 from .models import Alumnos
 from .models import Comentario
@@ -5,11 +6,15 @@ from .models import ComentarioContacto
 # Register your models here.
 class AdministrarModelo(admin.ModelAdmin):
  readonly_fields = ('created', 'updated')
- list_display = ('matricula', 'nombre', 'carrera','turno')
+ list_display = ('matricula', 'nombre', 'carrera','turno','created')
  search_fields = ('matricula','nombre','carrera','turno')
  date_hierarchy = 'created'
  list_filter = ('carrera','turno')
-
+ def get_readonly_fields(self,request,obj=None):
+    if  request.user.groups.filter(name="Usuarios").exists():
+        return ('matricula','carrera','turno')
+    else:
+        return('created', 'updated')
 admin.site.register(Alumnos,AdministrarModelo)
 
 class AdministrarComentarios(admin.ModelAdmin):
--- prueba/registros/views.py
+++ prueba/registros/views.py
@@ -3,18 +3,43 @@ from .models import Alumnos, Comentario, ComentarioContacto
 from .forms import ComentarioContactoForm
 from .forms import AlumnosForm
 from django.shortcuts import get_object_or_404
+import datetime
+
 # Create your views here.
 def registros(request):
  alumnos=Alumnos.objects.all()
  return render(request,"registros/principal.html",{'alumnos':alumnos})
 
+def consulta1(request):
+ alumnos=Alumnos.objects.filter(carrera="TI")
+ return render(request,"registros/consultas.html",{'alumnos':alumnos})
 
+def consulta2(request):
+ alumnos=Alumnos.objects.filter(carrera="TI").filter(turno="Matutino")
+ return render(request,"registros/consultas.html",{'alumnos':alumnos})
+def consulta3(request):
+ alumnos=Alumnos.objects.all().only("matricula","nombre","carrera","turno","imagen")
+ return render(request,"registros/consultas.html",{'alumnos':alumnos})
+def consulta4(request):
+ alumnos=Alumnos.objects.filter(turno__contains='Vesp')
+ return render(request,"registros/consultas.html",{'alumnos':alumnos})
+def consulta5(request):
+ alumnos=Alumnos.objects.filter(nombre__in=["Juan","Ana"])
+ return render(request,"registros/consultas.html",{'alumnos':alumnos})
+def consulta6(request):
+ fechai=datetime.date(2022,7,1)
+ fecha=datetime.date(2022,7,15)
+ alumnos=Alumnos.objects.filter(created__range=(fechai,fecha))
+ return render(request,"registros/consultas.html",{'alumnos':alumnos})
+def consulta7(request):
+ alumnos=Alumnos.objects.filter(comentario__coment__contains="es un")
+ return render(request,"registros/consultas.html",{'alumnos':alumnos})
 def eliminaralumno(request,matricula,confimacion='registros/conel2.html'):
     alumno = get_object_or_404(Alumnos, matricula=matricula)
     if request.method=='POST':
         alumno.delete()
         alumnos=Alumnos.objects.all()
-        return render(request,"registros/principal.html",{'alumno':alumnos})
+        return render(request,"registros/principal.html",{'alumnos':alumnos})
     return render (request, confimacion,{'object':alumno})
 
 def editaral(request,matricula,):
